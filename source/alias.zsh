80b2a909011d5f0d556484d57e172824367aa8b9
Matouš Jandek <mjandek@localhost.localdomain>
2019-04-08 11:37:56 +0200

    Added custom configuration files - Configuration for zsh and oh-my-zsh

---
+++ source/alias.zsh
@@ -0,0 +1,24 @@
+alias zshconfig="vim ~/.zshrc"
+alias ohmyzshconfig="vim ~/.oh-my-zsh"
+alias i3lock="i3lock \
+                  --indicator \
+		  --clock \
+		  --blur=10 \
+		  --keylayout 0 \
+		  --timecolor=8FA388FF \
+		  --datecolor=8FA388FF \
+		  --layoutcolor=8FA388FF \
+		  --wrongcolor=AF6B42FF \
+		  --verifcolor=567A6EFF \
+		  --insidecolor=1D2426FF \
+		  --insidevercolor=1D2426FF \
+		  --insidewrongcolor=1D2426FF \
+		  --ringcolor=8FA388FF \
+		  --ringvercolor=567A6EFF \
+		  --ringwrongcolor=AF6B42FF \
+		  --keyhlcolor=869151FF \
+		  --bshlcolor=B07350FF "
+
+# git aliases
+alias ggui="git gui &>/dev/null &"
+
