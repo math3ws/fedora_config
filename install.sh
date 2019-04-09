21241a01985370ab83ce0ca81d44a30731493b12
Matouš Jandek <mjandek@localhost.localdomain>
2019-04-09 10:47:40 +0200

    Updated install script - Better structure of the script. - Added update to package install section. - Added variables for paths used in commands etc. - Added setup of i3 as the session manager for the current user.

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
-# install packages
+#======================================
+# script variables
+#======================================
 
-dnf copr enable -y ianhattendorf/desktop
+# absolute path of this script file
+scriptpath=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)/$(basename -- "$0")")
+
+# resource files
+res_gdmuserfile=$scriptpath/res/accountservice
+
+#======================================
+# install packages
+#======================================
 
 packages="$packages curl"
 packages="$packages dmenu"
@@ -12,13 +22,31 @@ packages="$packages util-linux-user"
 packages="$packages vim-enhanced"
 packages="$packages zsh"
 
+dnf copr enable -y ianhattendorf/desktop
+dnf -y update
 dnf install -y $packages
 
-# shell setup
+#======================================
+# terminal setup
+#======================================
+
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# modify zshconfig
+#======================================
+# i3 setup
+#======================================
 
+# variant for gdm
+gdmuserfile=/var/lib/AccountService/users/$USERNAME
+cp $res_gdmuserfile $gdmuserfile
+echo "Icon=/home/$USERNAME/.face" >> $gdmuserfile
+
+#======================================
+# modify zshconfig
+#======================================
+
+#======================================
 # automount windows
+#======================================
 
