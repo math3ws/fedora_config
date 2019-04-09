77c8a42162050403b074ba7133e788b22a47e49e
Matouš Jandek <mjandek@localhost.localdomain>
2019-04-09 21:03:13 +0200

    Update permissions in install scripts

--- script/install.sh
+++ script/install.sh
@@ -5,6 +5,7 @@
 SCRIPTPATH=$(readlink -f "$0")
 SCRIPTDIRPATH=$(dirname "$SCRIPTPATH")
 RESOURCEPATH=$SCRIPTDIRPATH/installres
+SCRIPTUSER="who | cut -d \" \" -f1"
 
 #======================================
 # install packages
@@ -27,7 +28,7 @@ dnf install -y $PACKAGES
 # clone git repo
 #======================================
 
-git clone https://github.com/math3ws/fedora_config.git $RESOURCEPATH
+sudo -u $SCRIPTUSER git clone https://github.com/math3ws/fedora_config.git $RESOURCEPATH
 
 #======================================
 # run setup script
--- script/setup.sh
+++ script/setup.sh
@@ -5,13 +5,14 @@
 SCRIPTPATH=$(readlink -f "$0")
 SCRIPTDIRPATH=$(dirname "$SCRIPTPATH")
 RESOURCEPATH=$SCRIPTDIRPATH/res
+SCRIPTUSER="who | cut -d \" \" -f1"
 
 #======================================
 # terminal setup
 #======================================
 
-chsh -s /bin/zsh $USERNAME
-cd ~ && sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /bin/zsh $SCRIPTUSER
+cd ~ && sudo -u $SCRIPTUSER sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #======================================
 # i3 setup
@@ -19,9 +20,9 @@ cd ~ && sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master
 
 # variant for gdm
 GDMUSERFILETEMPLATE=$RESOURCEPATH/accountservice
-GDMUSERFILE=/var/lib/AccountService/users/$USERNAME
-cp $GDMUSERFILETEMPLATE $GDMUSERFILE
-echo "Icon=/home/$USERNAME/.face" >> $GDMUSERFILE
+GDMUSERFILE=/var/lib/AccountService/users/$SCRIPTUSER
+sudo -u $SCRIPTUSER cp $GDMUSERFILETEMPLATE $GDMUSERFILE
+sudo -u $SCRIPTUSER echo "Icon=/home/$SCRIPTUSER/.face" >> $GDMUSERFILE
 
 #======================================
 # modify zshconfig
