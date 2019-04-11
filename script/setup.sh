63c626eaddb7fc10d638731effb8599a8f5e505d
Matouš Jandek <mjandek@localhost.localdomain>
2019-04-11 09:01:23 +0200

    setup.sh improvements - Slight reorganization of the structure. - Added config file for i3, which will be used as user settings.

--- script/setup.sh
+++ script/setup.sh
@@ -10,11 +10,19 @@ RESOURCEDIR=$SCRIPTDIR/../res
 SCRIPTUSER=$(who | cut -d " " -f1)
 
 #======================================
-# terminal setup
+# general setup
+#======================================
+
+sudo -u $SCRIPTUSER cp "$RESOURCEDIR/Xresources" "/home/$SCRIPTUSER/.Xresources"
+
+#======================================
+# zsh setup
 #======================================
 
 chsh -s /bin/zsh $SCRIPTUSER
 cd ~ && sudo -u $SCRIPTUSER $SCRIPTDIR/ohmyzshsetup.sh 
+sudo -u $SCRIPTUSER cp "$RESOURCEDIR/zshrc" "/home/$SCRIPTUSER/.zshrc"
+sudo -u $SCRIPTUSER cp "$RESOURCEDIR/alias.zsh" "/home/$SCRIPTUSER/.oh-my-zsh/custom/alias.zsh"
 
 #======================================
 # i3 setup
@@ -25,14 +33,8 @@ GDMUSERFILETEMPLATE="$RESOURCEDIR/accountsservice"
 GDMUSERFILE="/var/lib/AccountsService/users/$SCRIPTUSER"
 cp $GDMUSERFILETEMPLATE $GDMUSERFILE
 echo "Icon=/home/$SCRIPTUSER/.face" >> $GDMUSERFILE
-
-#======================================
-# modify zshconfig
-#======================================
-
-sudo -u $SCRIPTUSER cp "$RESOURCEDIR/zshrc" "/home/$SCRIPTUSER/.zshrc"
-sudo -u $SCRIPTUSER cp "$RESOURCEDIR/alias.zsh" "/home/$SCRIPTUSER/.oh-my-zsh/custom/alias.zsh"
-sudo -u $SCRIPTUSER cp "$RESOURCEDIR/Xresources" "/home/$SCRIPTUSER/.Xresources"
+# generic steps
+sudo -u $SCRIPTUSER cp "$RESOURCEDIR/i3config" "/home/$SCRIPTUSER/.config/i3/config"
 
 #======================================
 # automount windows
