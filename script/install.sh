52ed39498cd28fb28503f940304beb805c5617b5
Matouš Jandek <mjandek@localhost.localdomain>
2020-06-15 20:44:07 +0200

    Refactored option parsing

--- script/install.sh
+++ script/install.sh
@@ -26,10 +26,48 @@ setupProgramSettings () {
 # argument parsing
 #======================================
 parseArguments() {
-    while getopts v: opt; do
-        case $opt in
-            v) $VERBOSE=1
-	        ;;
+    # saner programming env: these switches turn some bugs into errors
+    set -o errexit -o pipefail -o noclobber -o nounset
+
+    # -allow a command to fail with !’s side effect on errexit
+    # -use return value from ${PIPESTATUS[0]}, because ! hosed $?
+    ! getopt --test > /dev/null
+    if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+        echo '`getopt --test` failed in this environment'
+        return 1
+    fi
+
+    OPTIONS=v
+    LONGOPTS=verbose
+
+    # -regarding ! and PIPESTATUS see above
+    # -temporarily store output to be able to check for errors
+    # -activate quoting/enhanced mode (e.g. by writing out “--options”)
+    # -pass arguments only via   -- "$@"   to separate them correctly
+    ! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+        # e.g. return value is 1
+        #  then getopt has complained about wrong arguments to stdout
+        return 1
+    fi
+    # read getopt’s output this way to handle the quoting right:
+    eval set -- "$PARSED"
+
+    # now enjoy the options in order and nicely split until we see --
+    while true; do
+        case "$1" in
+            -v|--verbose)
+                v=1
+                shift
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)
+                echo "Unknown option passed to install.sh"
+                return 2
+                ;;
         esac
     done
 }
